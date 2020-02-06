dadd35ac40cc69e15ea7044f91d896bf7656615f
bryantgunaman <bryantgunaman@gmail.com>
2020-02-06 12:33:39 -0800

    started on wrapper

--- Malmo-0.37.0-Mac-64bit_withBoost_Python3.7/ZombieKiller/main_keras.py
+++ Malmo-0.37.0-Mac-64bit_withBoost_Python3.7/ZombieKiller/main_keras.py
@@ -1,40 +1,83 @@
+from __future__ import print_function
+import malmo.minecraftbootstrap; malmo.minecraftbootstrap.set_malmo_xsd_path()
+from future import standard_library
+standard_library.install_aliases()
+from builtins import input
+from builtins import range
+from builtins import object
+from malmo import MalmoPython
 from simple_dqn_keras import Agent
-
+import json
+import logging
+import math
+import os
+import random
+import sys
+import time
+from malmo import malmoutils
 import numpy as np
-#from utils impot plotLearning
-import gym
+
+
+class MainKeras():
+
+    def __init__(self, missionXML, n_games=500, max_retries=3):
+        self.n_games = n_games
+        self.agent = Agent(gamma=0.99, epsilon=1.0, alpha=0.0005, input_dims=8,
+                  n_actions=4, mem_size=1000000, batch_size=64, epsilon_end=0.01)
+        self.scores = []
+        self.eps_history = []
+        agent_host = MalmoPython.AgentHost()
+
+        try:
+            agent_host.parse( sys.argv )
+        except RuntimeError as e:
+            print('ERROR:',e)
+            print(agent_host.getUsage())
+            exit(1)
+        if agent_host.receivedArgument("help"):
+            print(agent_host.getUsage())
+            exit(0)
+
+        print(missionXML)
+        self.my_mission = MalmoPython.MissionSpec(missionXML, True)
+        self.my_mission_record = MalmoPython.MissionRecordSpec()
+
+        self.max_retries = max_retries
+
+        self._add_starters()
+        self._add_default_client()
+
+    def _add_starters(self):
+        my_mission.removeAllCommandHandlers()
+        my_mission.allowAllDiscreteMovementCommands()
+        my_mission.requestVideo( 320, 240 )
+        my_mission.setViewpoint( 1 )
+
+    def _add_default_client(self):
+        my_clients = MalmoPython.ClientPool()
+        my_clients.add(MalmoPython.ClientInfo('127.0.0.1', 10000))
+
+
+
+    # def run(self):
+    #     for i in range(self.n_games):
+    #         while 
+
+
+
 
 if __name__ == '__main__':
-    env = gym.make('LunarLander-v2')
-    n_games = 500
-    agent = Agent(gamma=0.99, epsilon=1.0, alpha=0.0005, input_dims=8,
-                  n_actions=4, mem_size=1000000, batch_size=64, epsilon_end=0.01)
+    with open('default_flat_1.xml', 'r') as file:
+        mission_file = file.read().replace('\n', '')
+        # print(data)
 
-    #agnet.load_model()
-    scores = []
-    eps_history = []
+        mk = MainKeras(mission_file, n_games=500, max_retries=3)
 
-    for i in range(n_games):
-        done = False
-        score = 0
-        observation = env.reset()
-        while not done:
-            action = agent.choose_action(observation)
-            observation_, reward, done, info = env.step(action)
-            score += reward
-            agent.remember(observation, action, reward, observation, done)
-            observation = observation_
-            agent.learn()
 
-        eps_history.append(agent.epsilon)
-        scores.append(score)
 
-        avg_score = np.mean(scores[max(0, i-100):(i+1)])
-        print('episode ', 1, 'score %.2f' % score, 'average score %.2f' % avg_score)
+    
+    
 
-        if i%10 == 0 and i > 0:
-            agent.save_model()
-        
-    filename = 'lunar_lander.png'
-    x = [i+1 for i in range(n_games)]
-    #plotLearning(x, scores, eps_history, filename)
+    
+
+
