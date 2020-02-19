41bb96a65e3ea698dd3a8482bcedd0fdc4193c6e
bryantgunaman <bryantgunaman@gmail.com>
2020-02-18 21:26:13 -0800

    added TimeAlive reward and loaded_model in constructor

--- Malmo-0.37.0-Mac-64bit_withBoost_Python3.7/ZombieKiller/dqn_zombie.py
+++ Malmo-0.37.0-Mac-64bit_withBoost_Python3.7/ZombieKiller/dqn_zombie.py
@@ -2,6 +2,6 @@ from main_keras import MainKeras
 
 
 if __name__ == '__main__':
-    mk = MainKeras('zombie_kill_1.xml', n_games=25, max_retries=3, 
-                    starting_zombies=1, XSize=10, ZSize=10)
+    mk = MainKeras('zombie_kill_1.xml', n_games=10, max_retries=3, 
+                    starting_zombies=2, XSize=10, ZSize=10, load_model=True)
     mk.run_dqn()
